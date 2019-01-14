577e5bdbbfaf5f3bd26bf14922bdacff9bcba9c2
Anum Asif <anum@cockar.com>
2019-01-14 14:19:33 +0300

    Add a function to delete a quote from the list of quotes

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
-
+  @Input() quote:Quote;
+  @Output() isInappropriate = new EventEmitter<boolean>();
+  quoteDelete(inappropriate:boolean){
+    this.isInappropriate.emit(inappropriate);
+  }
   constructor() { }
 
   ngOnInit() {
