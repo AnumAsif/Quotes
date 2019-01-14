e97fb9851151cc25a94945efb068aa111348ffe3
Anum Asif <anum@cockar.com>
2019-01-14 14:25:42 +0300

    Add buttons to collect likes and dislikes for each quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,4 @@
 export class Quote {
+    constructor(public id:number, public publisher:string, public author:string, public quoteWords:string, public timeOfCreation:Date,
+    public upVotes:number, public downVotes:number){}
 }
