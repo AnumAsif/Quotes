f96dc31e2c964f935c82098952fd57b7fa261e8d
Anum Asif <anum@cockar.com>
2019-01-16 10:20:27 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,4 +0,0 @@
-export class Quote {
-    constructor(public id:number, public publisher:string, public author:string, public quoteWords:string, public timeOfCreation:Date,
-    public upVotes:number, public downVotes:number){}
-}
